537be7adc24cc454099517986339e236b94ec931
Novikov <test@test.ru>
2026-04-30 13:58:36 +0700

    Лабораторная работа №2 готова

---
+++ backend/schemas/status_schema.py
@@ -0,0 +1,9 @@
+from apiflask import Schema
+from apiflask.fields import String, Integer
+
+
+class Status0utSchema(Schema):
+    status = String()
+    message = String()
+    service = String()
+    items_count = Integer()
